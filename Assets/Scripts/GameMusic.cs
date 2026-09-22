b79ef4333e7e47b5c815c2b1dd9fe236941bed79
Mike Petanitis <aypnos_eros@hotmail.com>
2026-09-22 12:54:49 +0300

    Add Block Breaker C# scripts

---
+++ Assets/Scripts/GameMusic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMusic : MonoBehaviour {
+	static GameMusic instance = null;
+	
+	void Awake () {
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate game music self-destructing");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
